29e7233aa29538f7f3abe0fae404bfdef9d084a0
Francisco Anjos <fcariaanjos@gmail.com>
2023-05-19 10:51:25 +0100

    Created CompareByName

--- PlayerManager4/CompareByName.cs
+++ PlayerManager4/CompareByName.cs
@@ -1,4 +1,20 @@
 namespace PlayerManager4
 {
-    
+    public class CompareByName : IComparer<Player>
+    {
+        private readonly bool ascending;
+        public CompareByName(bool ascending)
+        {
+            this.ascending = ascending;
+        }
+        public int Compare(Player x, Player y)
+        {
+            int result =string.Compare(x.name, y.name);
+
+            if (!ascending)
+                result *= -1;
+
+            return result;
+        }
+    }
 }
