0799723832090b912dae336f964e4d9535e50d4a
Francisco Anjos <fcariaanjos@gmail.com>
2023-05-19 10:35:46 +0100

    correct namespace

---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,18 @@
+namespace PlayerManager3
+{
+    public class Player :IComparable<Player>
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        public int CompareTo(Player other)
+        {
+            return other.Score.CompareTo(Score);
+        }
+    }   
+}
